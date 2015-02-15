f30c14403e70d56075dd0e0461e0b401e6c2a38d
Tomer <t.gabay@student.rug.nl>
2015-02-15 20:44:40 +0100

    vlag word eenmalig random ge-genereerd

--- flag_ui.py
+++ flag_ui.py
@@ -11,60 +11,50 @@ import sys
 
 class Interface(QtGui.QWidget):
 
-    def __init__(self):
-        super(Interface,self).__init__()
-        self.initUI()
+	def __init__(self):
+		super(Interface,self).__init__()
+		self.initUI()
 
-    def initUI(self):
-        #button 1
-        self.btn=QtGui.QLabel('Land',self)
-        self.btn.move(30,30)
+	def initUI(self):
+		#button 1
+		self.btn=QtGui.QLabel('Land',self)
+		self.btn.move(30,30)
 
-        #dropdownmenu
-        self.combo = QtGui.QComboBox(self)
-        self.combo.move(5, 50)
-        self.combo.resize(120,30)
+		#dropdownmenu
+		self.combo = QtGui.QComboBox(self)
+		self.combo.move(5, 50)
+		self.combo.resize(120,30)
 
-        # hier wordt het dropdownmenu gemaakt via een list
-        # lege lijst en alle items appenden, deze lijst adden
-        lijst=[]
-        with open('countries_list.txt') as in_f:
-            for line in in_f:
-                x = line.split('\n')
-                lijst.append(x)
+		# hier wordt het dropdownmenu gemaakt via een list
+		# lege lijst en alle items appenden, deze lijst adden
+		lijst=[]
+		with open('countries_list.txt') as in_f:
+			for line in in_f:
+				x = line.split('\n')
+				lijst.append(x)
 
-        lijst2=[]
-        for i in lijst:
-            lijst2.append(i[0])
-        print(lijst2)
-        self.combo.addItems(lijst2)
-        self.combo.activated.connect(self.drawRectangles)
+		lijst2=[]
+		for i in lijst:
+			lijst2.append(i[0])
+		print(lijst2)
+		self.combo.addItems(lijst2)
+		self.combo.activated.connect(self.paintEvent)
 
-        self.setGeometry(200,200,300,300)
-        self.setWindowTitle("Vlaggen")
-        self.show()
+		self.setGeometry(200,200,300,300)
+		self.setWindowTitle("Vlaggen")
+		self.show()
 
-    def drawRectangles(self,event):
-        # bij het aangeklikte item wordt de bijhorende vlag random opgezocht
-        #self.gekozen=self.combo.currentText()
+	def paintEvent(self, e):
+		#activates a pain-Event
+		qp = QtGui.QPainter()
+		qp.begin(self)
+		self.drawRectangles(qp)
+		qp.end()
 
-        qp = QtGui.QPainter()
-        qp.begin(self)
-
-        color = QtGui.QColor(0, 0, 0)
-        color.setNamedColor('#d4d4d4')
-        qp.setPen(color)
-
-        qp.setBrush(QtGui.QColor(200, 0, 0))
-        qp.drawRect(10, 15, 90, 60)
-
-        qp.setBrush(QtGui.QColor(255, 80, 0, 160))
-        qp.drawRect(130, 15, 90, 60)
-
-        qp.setBrush(QtGui.QColor(25, 0, 90, 200))
-        qp.drawRect(250, 15, 90, 60)
-
-        qp.end()
+	def drawRectangles(self, qp):
+		#Choose random color and paint flag
+		qp.setBrush(QtGui.QColor(randrange(0,256,1),randrange(0,256,1), randrange(0,256,1)))
+		qp.drawRect(10, 80, 90, 60)
 
 if __name__ == '__main__':
     app=QtGui.QApplication(sys.argv)
