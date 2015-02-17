7d6132b4ddb4cf936fcfd25cc933532679dc040a
Laurence <l.h.a.arnold@student.rug.nl>
2015-02-17 16:12:27 +0100

    [Commentaar gemaakt van regel 33]

--- flag_ui.py
+++ flag_ui.py
@@ -1,7 +1,4 @@
-#!/opt/local/bin/python3.4
-#voor linux
-#!usr/bin/env python
-
+#!/usr/bin/env python
 
 from PyQt4 import QtGui, QtCore
 
@@ -19,49 +16,48 @@ class Interface(QtGui.QWidget):
 		#button 1
 		self.btn=QtGui.QLabel('Land',self)
 		self.btn.move(30,30)
-
-		#dropdownmenu
+		
 		self.combo = QtGui.QComboBox(self)
 		self.combo.move(5, 50)
 		self.combo.resize(120,30)
-
-		# hier wordt het dropdownmenu gemaakt via een list
-		# lege lijst en alle items appenden, deze lijst adden
+		
 		lijst=[]
 		with open('countries_list.txt') as in_f:
 			for line in in_f:
 				x = line.split('\n')
 				lijst.append(x)
-
 		lijst2=[]
 		for i in lijst:
-			lijst2.append(i[0])
-		self.countryDic = {}
-		for country in lijst2:
-			self.countryDic[country] = country
+			lijst2.append(i[0])				
 		self.combo.addItems(lijst2)
-		self.combo.activated.connect(self.paintEvent)
-
+		#self.combo.activated.connect(self.paintEvent)
+		
+		self.countryDic={}
+		for country in lijst2:
+			self.countryDic[country]=country		
+		
 		self.setGeometry(200,200,300,300)
-		self.setWindowTitle("Vlaggen")
-		self.show()
+		self.setWindowTitle("Vlaggen bij een Land!")
+		self.show()		
 
-	def paintEvent(self, e):
+	def paintEvent(self, event):
 		#activates a pain-Event
 		qp = QtGui.QPainter()
 		qp.begin(self)
-		self.drawRectangles(qp)
-		qp.end()
-
-	def drawRectangles(self, qp):
-		#Choose random color and paint flag
 		for country in self.countryDic:
 			qp.setBrush(QtGui.QColor(randrange(0,256,1),randrange(0,256,1), randrange(0,256,1)))
 			qp.drawRect(10, 80, 90, 60)
-			self.countryDic[country] = self.countryDic[country],qp
-
+			self.countryDic[country]=self.countryDic[country], qp
+		
+		qp.end()
+	
+	#def drawRectangles(self,event,qp):						
+		
+		#print(self.countryDic)
+		
+	
 if __name__ == '__main__':
     app=QtGui.QApplication(sys.argv)
-    ex=Interface()
-    ex.show()
+    interface=Interface()
+    interface.show()
     app.exec_()
