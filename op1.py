374b7c9f6253304bfc52e5a578114b9a658a0174
S2702150 <s2702150@let-edu-145-71.let.rug.nl>
2015-02-18 09:58:52 +0100

    [tabs gefixt]

--- op1.py
+++ op1.py
@@ -1,28 +1,25 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import sys
 
 from country import *
 
 def main():
-    #for land in open("countries_list.txt","r"):
-     #   country=Country(land)
-      #  print(country)
+	lijst=[]
 
-    lijst=[]
-    with open('countries_list.txt') as in_f:
-        for line in in_f:
-            x = line.split('\n')
-            lijst.append(x)
+	with open('countries_list.txt') as in_f:
+		for line in in_f:
+			x = line.split('\n')
+			lijst.append(x)
 
-    lijst2=[]
-    for i in lijst:
-        lijst2.append(i[0])
+	lijst2=[]
+	for i in lijst:
+		lijst2.append(i[0])
 
- 	countryDic = {}
+	countryDic={}
 	for country in lijst2:
-		countryDic[country] = Country(country)
+		countryDic[country]=Country(country)
 	print (countryDic)
 
 if __name__== "__main__":
-    main()
+	main()
