5f5658c7bd40a0fb7c153b030c447e4a8b8f76d4
S2702150 <s2702150@let-edu-145-71.let.rug.nl>
2015-02-18 09:57:45 +0100

    [Update]

--- flag_color.py
+++ flag_color.py
@@ -8,7 +8,13 @@ class FlagColor(QtGui.QColor):
     def __init__(self):
         super(FlagColor,self).__init__()
 
-    def kleurvlag(self,getal):
+    def kleurvlag(self):
+		self.setRed(randrange(0,256))
+		self.setGreen(randrange(0,256))
+		self.setBlue(randrange(0,256))
+		
+		return self.SetRed,self.setGreen,self.setBlue
+		
         
 if __name__== "__main__":
     main()
